4c393c46bcb3b4a70220157b968ae3256f08ce7b
Jovenko Dmitriy <iovenko.dima30@gmail.com>
2026-05-09 17:32:40 +0700

    test-ci: Introduced a bug to test the performance of GTest and the CI pipeline.

--- src/Calculator.cpp
+++ src/Calculator.cpp
@@ -4,7 +4,7 @@
 
 int Calculator::Add(int a, int b) { return a + b; }
 int Calculator::Subtract(int a, int b) { return a - b; }
-int Calculator::Multiply(int a, int b) { return a * b; }
+int Calculator::Multiply(int a, int b) { return a + b; }
 double Calculator::Divide(int a, int b) {
   if (b == 0) {
     throw std::invalid_argument("Division by zero is not allowed.");
